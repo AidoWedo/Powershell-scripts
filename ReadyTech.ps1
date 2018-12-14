2c2cb0844e976d6951840dac63173fbf9242cc8f
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2018-12-14 10:51:42 +0000

    Add files via upload

---
+++ ReadyTech.ps1
@@ -0,0 +1,27 @@
+﻿# Initialise Logger
+Initialize-logger -LoggerName "ReadyTeachViewer" -loglevel debug
+# Read ReadyTech rt_deployment File for Values
+$path = "c:\rt_deployment.txt"
+$values = [pscustomobject](Get-Content $path -Raw | ConvertFrom-StringData)
+
+# ReadyTech Service Monitoring and Restart of Viewer Service
+$ServiceName = 'ReadyTech Viewer Server Service'
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -ne 'Running')
+{
+
+    # Start-Service $ServiceName
+    Restart-Service $ServiceName -force 
+    Get-Service $ServiceName | Select-Object -expand dependentservices | Start-Service
+    write-LogTitle "ReadyTech Viewer Status" | write-LogObject ($arrService.status)
+    Write-LogTitle "ReadyTech Viewer Starting" | write-LogObject ($ServiceName)
+    Start-Sleep -seconds 60
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
+    {
+        Write-Host $ServiceName "Service is now Running"
+    }
+
+}
+
---
+++ ReadyTechWritetoEventLog.ps1
@@ -0,0 +1,3 @@
+﻿# Need to run this once to create the source
+# New-Eventlog -LogName Application -Source "Remote Students ReadyTech Viewer Service Restarted"
+Write-EventLog -LogName Application -Source "Remote Students ReadyTech Viewer Service Restarted" -EntryType Warning -EventID 1 -Message "Student Machine For $($values.activation_name00) the ReadyTech Viewer Service has been restarted"
