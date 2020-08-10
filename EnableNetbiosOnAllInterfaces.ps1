d8c24533e8507c9db96d48489bf2e34dee8b8fd3
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2020-08-10 07:46:03 +0100

    Add files via upload
    Added Powershell scripts for setting NetBios on and for updating the DNS record of an Adapters address and updating a set of domain records

---
+++ EnableNetbiosOnAllInterfaces.ps1
@@ -0,0 +1,8 @@
+﻿# Author Richard Bevan
+# Set All Adapter to have NetBios Enabled
+# Each Virtual Machine in the 7.5 306 Class has different Interface address so rather than figure it out each time, blast those settings everywhere - abit lazy
+$adapters=(gwmi win32_networkadapterconfiguration )
+Foreach ($adapter in $adapters){
+  Write-Host $adapter
+  $adapter.settcpipnetbios(1)
+  }
---
+++ UpdateDNSRecordforlrvmad.ps1
@@ -0,0 +1,40 @@
+﻿# Update DNS Record to be correct ip address
+# Update various DNS records to point to IP address
+# Whats the problem on a lab machine without a static ip address, each time the machine comes up the records don't match the DNS records.
+# NetLogon will fix some of them but not all of them
+# Assumptions Machine is called lrvm-ad change it
+# Assumption 2 Domain is called bbte.local
+# Author Richard Bevan
+$oldobj = get-dnsserverresourcerecord -name lrvm-ad -ZoneName bbte.local -RRType A
+$newobj = get-dnsserverresourcerecord -name lrvm-ad -ZoneName bbte.local -RRType A
+$updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
+# Update Host Record however this should be done by netlogon service
+set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
+# Update Server DNS address
+Set-DnsClientServerAddress -InterfaceIndex 9 -ServerAddress ($updateip)
+# Lets wait a bit
+Start-Sleep -s 15
+# Update DNS records for SRV Record DomnainDNSZones, ForestDNSZones and gc._msdcs
+# Update gc._msdcs  first
+$oldobj = get-dnsserverresourcerecord -name "gc._msdcs" -ZoneName bbte.local -RRType A
+$newobj = get-dnsserverresourcerecord -name "gc._msdcs" -ZoneName bbte.local -RRType A
+$updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
+set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
+# Lets wait a bit
+Start-Sleep -s 15
+# Update ForestDnsZone first
+$oldobj = get-dnsserverresourcerecord -name "ForestDnsZones" -ZoneName bbte.local -RRType A
+$newobj = get-dnsserverresourcerecord -name "ForestDnsZones" -ZoneName bbte.local -RRType A
+$updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
+set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
+# Lets wait a bit
+# Update DomainDnsZone first
+Start-Sleep -s 15
+$oldobj = get-dnsserverresourcerecord -name "DomainDnsZones" -ZoneName bbte.local -RRType A
+$newobj = get-dnsserverresourcerecord -name "DomainDnsZones" -ZoneName bbte.local -RRType A
+$updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
+set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
