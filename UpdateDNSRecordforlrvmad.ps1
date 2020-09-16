63013c7605ca0b53020ea40d38c698b2543b3eb7
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2020-09-16 10:43:24 +0100

    Add files via upload

--- UpdateDNSRecordforlrvmad.ps1
+++ UpdateDNSRecordforlrvmad.ps1
@@ -1,9 +1,4 @@
 ﻿# Update DNS Record to be correct ip address
-# Update various DNS records to point to IP address
-# Whats the problem on a lab machine without a static ip address, each time the machine comes up the records don't match the DNS records.
-# NetLogon will fix some of them but not all of them
-# Assumptions Machine is called lrvm-ad change it
-# Assumption 2 Domain is called bbte.local
 # Author Richard Bevan
 $oldobj = get-dnsserverresourcerecord -name lrvm-ad -ZoneName bbte.local -RRType A
 $newobj = get-dnsserverresourcerecord -name lrvm-ad -ZoneName bbte.local -RRType A
@@ -12,7 +7,7 @@ $newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
 # Update Host Record however this should be done by netlogon service
 set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
 # Update Server DNS address
-Set-DnsClientServerAddress -InterfaceIndex 9 -ServerAddress ($updateip)
+Set-DnsClientServerAddress -InterfaceIndex 10 -ServerAddress ($updateip)
 # Lets wait a bit
 Start-Sleep -s 15
 # Update DNS records for SRV Record DomnainDNSZones, ForestDNSZones and gc._msdcs
@@ -35,6 +30,13 @@ set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -Zon
 Start-Sleep -s 15
 $oldobj = get-dnsserverresourcerecord -name "DomainDnsZones" -ZoneName bbte.local -RRType A
 $newobj = get-dnsserverresourcerecord -name "DomainDnsZones" -ZoneName bbte.local -RRType A
+$updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
+$newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
+set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
+Start-Sleep -s 15
+# Update @ record
+$oldobj = get-dnsserverresourcerecord -name "@" -ZoneName bbte.local -RRType A | Where-Object TimeToLive -EQ "00:10:00"
+$newobj = get-dnsserverresourcerecord -name "@" -ZoneName bbte.local -RRType A | Where-Object TimeToLive -EQ "00:10:00"
 $updateip = (Test-connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString
 $newobj.recorddata.ipv4address=[System.Net.IPAddress]::parse($updateip)
 set-dnsserverresourcerecord -NewInputObject $newobj -OldInputObject $oldobj -ZoneName bbte.local -passthru
